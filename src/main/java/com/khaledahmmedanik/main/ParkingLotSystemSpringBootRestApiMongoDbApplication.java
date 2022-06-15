1fc2d69a081e20b53d1da8a0ca9de39798a5893e
KHALED AHMMED ANIK <59437962+Khaled227@users.noreply.github.com>
2022-06-15 16:02:41 +0600

    Project Initialized

---
+++ src/main/java/com/khaledahmmedanik/main/ParkingLotSystemSpringBootRestApiMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.khaledahmmedanik.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLotSystemSpringBootRestApiMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingLotSystemSpringBootRestApiMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khaledahmmedanik/main/ParkingLotSystemSpringBootRestApiMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khaledahmmedanik.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotSystemSpringBootRestApiMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
